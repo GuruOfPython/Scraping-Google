497d9c02ea8e9605d7604dc1e1453f58fb553d9a
GuruOfPython <vpeKJmPc@mail.com>
2018-12-28 09:29:50 -0800

    Update of utils

--- utils.py
+++ utils.py
@@ -1,11 +1,25 @@
 import re
+from urllib.parse import urlparse, parse_qs, urlencode, parse_qsl, urlunsplit
+
 
 def get_url(raw_url):
     match = re.findall(r"url\(\'([^)^\']+)", raw_url)[0]
-    if "https" not in match:
-        match = "http:" + match
-    return match
 
+    url_parsed = urlparse(match)
+    print(url_parsed)
+    scheme = 'http'
+    netloc = url_parsed.netloc
+    path = url_parsed.path
+    query = url_parsed.query
+    fragment = url_parsed.fragment
+    query_parsed = dict(parse_qsl(query))
+
+    query_parsed['w'] = '600'
+    query_parsed['h'] = '400'
+    query_update = urlencode(query_parsed)
+
+    new_url = urlunsplit((scheme, netloc, path, query_update, fragment))
+    return new_url
 
 
 raw_url = "background-image:-webkit-linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.5)),url('//geo1.ggpht.com/cbk?panoid=PlKo-BPkSGkjVeifwnt6_g&output=thumbnail&cb_client=search.LOCAL_UNIVERSAL.gps&thumb=2&w=158&h=78&yaw=263.92426&pitch=0&thumbfov=100');height:78px;width:158px"
